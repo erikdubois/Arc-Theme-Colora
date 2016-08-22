0fb028bb90db56d32cdac6bad0975dc0d6dfae3b
Erik Dubois <erik.dubois@gmail.com>
2016-08-22 17:53:07 +0200

    Commit comment : color script on Mon Aug 22 17:53:07 CEST 2016

---
+++ change-color.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+############################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+############################################################################
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+############################################################################
+
+
+# changing the assets from colour
+
+# Mint-Y Theme 
+# Light green - 9ab87c
+# Dark green - 8fa876
+
+# but there are some minor variations to consider as well
+
+# put your colour codes in here and do not change the rest
+
+oldcolour1=c77320
+newcolour1=ecedef
+
+
+find common -name "*.*" -type f -exec sed -i 's/'$oldcolour1'/'$newcolour1'/g' {}  \;
+
+
