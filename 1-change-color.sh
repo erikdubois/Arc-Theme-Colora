1d786a475c391d395ced7941be441a1de052c764
Erik Dubois <erik.dubois@gmail.com>
2016-10-05 20:41:55 +0200

    Commit comment :  on Wed Oct  5 20:41:55 CEST 2016

--- 1-change-color.sh
+++ 1-change-color.sh
@@ -1,56 +1,13 @@
 #!/bin/bash
 #
-#                                       
-# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
-# MMm----::-://////////////oymNMd+`     
-# MMd      /++                -sNMd:    
-# MMNso/`  dMM    `.::-. .-::.` .hMN:   
-# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
-#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  .mmd  `mmm   yMM. dMM   
-#     NMm  dMM`  ..`   ...   ydm. dMM   
-#     hMM- +MMd/-------...-:sdds  dMM   
-#     -NMm- :hNMNNNmdddddddddy/`  dMM   
-#      -dMNs-``-::::-------.``    dMM   
-#       `/dMNmy+/:-------------:/yMMM  
-#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
-#             \.MMMMMMMMMMMMMMMMMMM    
-#                                      
-#
-#
 ##################################################################################################################
 # Written to be used on 64 bits computers
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ##################################################################################################################
-# 
-# More from Erik Dubois
-#
-# Aurora Conky
-# at http://sourceforge.net/projects/auroraconkytheme/
-# Explanation on the use of this theme can be found at 
-# http://erikdubois.be/category/linux/aurora-conky/
-# 
-# Aureola Conky
-# Collections of nice conky's with lua syntax
-# https://github.com/erikdubois/Aureola
-#
-# Sardi icons
-# Many different styles of icons from colourfull, monochrome, white, circle
-# https://sourceforge.net/projects/sardi/
-#
-# Super Ultra Flat Numix Remix
-# Colourfull and playfull icons
-# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
-#
-# Check out the github - many more scripts for automatic installation of Linux Systems.
-#
-#
 ##################################################################################################################
 #
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
@@ -63,11 +20,16 @@ echo "Wait for it ...."
 # These lines will make sure all hex codes are in capital letters
 # This the colour blue (5294e2) from github Arc
 # DO NOT CHANGE THESE LINES
+# DO NOT CHANGE THESE LINES
+# DO NOT CHANGE THESE LINES
 oldcolour1=5294e2
 newcolour1=5294E2
 find common -name "*.*" -type f -exec sed -i 's/'$oldcolour1'/'$newcolour1'/g' {}  \;
 # Changing the colour blue into ...
 # Change the colour of newcolour1 ONLY
+# DO NOT CHANGE THESE LINES
+# DO NOT CHANGE THESE LINES
+# DO NOT CHANGE THESE LINES
 oldcolour1=5294E2
 #colour orange = sun = C77320
 
